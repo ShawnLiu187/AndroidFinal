1eef1b3f964101b834de40822bf3cb73645a4b80
ShawnLiu187 <31958595+ShawnLiu187@users.noreply.github.com>
2018-11-21 17:07:35 -0500

    nov 21 milestone 1

--- app/src/main/java/com/algonquincollege/liu00415/androidfinal/MainActivity.kt
+++ app/src/main/java/com/algonquincollege/liu00415/androidfinal/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.algonquincollege.liu00415.androidfinal
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val foodButton = findViewById<Button>(R.id.foodPortal)
+        foodButton.setOnClickListener {
+            val foodList = Intent(this, foodList::class.java)
+            startActivity(foodList)
+        }
+
+        val newsButton = findViewById<Button>(R.id.newsPortal)
+        newsButton.setOnClickListener {
+            val newsList = Intent(this, newsList::class.java)
+            startActivity(newsList)
+        }
+
     }
 }
