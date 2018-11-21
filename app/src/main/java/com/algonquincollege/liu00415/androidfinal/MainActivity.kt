dbc1aa9d7c8236bb691d9644ed53f88aa3e4919e
mccr0124 <32398053+mccr0124@users.noreply.github.com>
2018-11-21 16:29:52 -0500

    First Commit

--- app/src/main/java/com/algonquincollege/liu00415/androidfinal/MainActivity.kt
+++ app/src/main/java/com/algonquincollege/liu00415/androidfinal/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.algonquincollege.liu00415.androidfinal
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var busPortal = findViewById<Button>(R.id.busPortal)
+        busPortal.setOnClickListener{
+            var busActivity = Intent(this, OCTranspo::class.java)
+            startActivity(busActivity)
+        }
     }
 }
